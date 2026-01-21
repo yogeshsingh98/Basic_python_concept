eb3d181b48724eb250b273b57a43e38d3f1da2d1
yogeshsingh98 <yogesh998444@gmail.com>
2026-01-21 13:28:36 +0530

    Using fstring

--- greeting.py
+++ greeting.py
@@ -1,3 +1,3 @@
 FirstName =input("Enter Your FirstName ")
 LastName =input("Enter Your LastName ")
-print("Hello,",FirstName+" "+LastName,"! Welcome to the python program")
+print(f"Hello,{FirstName} {LastName}! Welcome to the python program")
